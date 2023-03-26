e63b02b896f31ff241b3ef2c1b73b113056cbe80
blueskin8 <garciaalbanrot@gmail.com>
2023-03-26 22:12:05 +0200

    update
    des trucs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BetterWindows
 Une amélioration de Windows : amélioration des interfaces, rajout de fonctionnalités ...
+
+# Informations
+## Cette version n'est pas le code source de BetterWindows. C'est juste le code source de l'installeur de BetterWindows
---
+++ index.js
@@ -0,0 +1,31 @@
+const electron = require('electron')
+
+const app = electron.app
+
+const createWindow = () => {
+    const win = new electron.BrowserWindow({
+        width: 960,
+        height: 540,
+        frame: false,
+        resizable: false,
+        roundedCorners: true,
+        movable: false,
+        icon: "./src/assets/icon.ico"
+    })
+    win.loadFile("./src/mainPage/mainPage.html")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+exports.close = () => app.exit(0)
+module.exports.closeApp = () => app.exit(0)
+
+electron.ipcMain.on('request-mainprocess-action', (event, arg) => {
+    console.log(arg)
+})
